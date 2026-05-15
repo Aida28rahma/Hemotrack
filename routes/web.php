6388d8561535749aca22adefad77e942af6c1479
Aida28rahma <aidarahma471@gmail.com>
2026-05-15 19:12:24 +0700

    Feat : aktivasi role

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,23 @@ Route::get('/', function () {
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/dashboard', function () {
-        return view('Petugas.dashboard');
-    })->name('dashboard');
+   Route::get('/dashboard', function () {
+
+    if (auth()->user()->role == 'dokter') {
+        return view('Dokter.dashboardDokter');
+    }
+    return view('Petugas.dashboard');
+    })->middleware(['auth'])->name('dashboard');
+
 
     Route::get('/stok', function () {
         return view('Petugas.stok');
     })->name('stok');
 
     Route::get('/permintaan', function (Request $request) {
+        if (auth()->user()->role != 'petugas') {
+        abort(403);
+        }
 
         $query = PermintaanDokter::query();
 
@@ -53,18 +61,34 @@ Route::middleware(['auth'])->group(function () {
     })->name('permintaan');
 
     Route::get('/distribusi', function () {
+        if (auth()->user()->role != 'petugas') {
+        abort(403);
+        }
+
         return view('Petugas.distribusi');
     })->name('distribusi');
 
     Route::get('/laporan', function () {
+        if (auth()->user()->role != 'petugas') {
+        abort(403);
+        }
+
         return view('Petugas.laporan');
     })->name('laporan');
 
     Route::get('/pmi', function () {
+        if (auth()->user()->role != 'petugas') {
+        abort(403);
+        }
+
         return view('Petugas.pmi');
     })->name('pmi');
 
     Route::get('/unit-bank-darah', function () {
+        if (auth()->user()->role != 'petugas') {
+        abort(403);
+        }
+
         return view('Petugas.unitBankDarah');
     })->name('unitBankDarah');
 
@@ -76,10 +100,22 @@ Route::middleware(['auth'])->group(function () {
     */
 
     // Dashboard dokter
-    Route::get('/dashboardDokter', fn() => view('dashboardDokter'))->name('dashboardDokter');
+    Route::get('/dashboardDokter', function () {
+        if (auth()->user()->role != 'dokter') {
+        abort(403);
+        }
+
+        return view('dashboardDokter');
+    })->name('Dokter.dashboardDokter');
 
     // Form permintaan dokter (GET)
-    Route::get('/permintaanDokter', fn() => view('permintaanDokter'))->name('permintaanDokter');
+    Route::get('/permintaanDokter', function () {
+        if (auth()->user()->role != 'dokter') {
+        abort(403);
+        }
+
+        return view('Dokter.permintaanDokter');
+    })->name('permintaanDokter');
 
     // SIMPAN PERMINTAAN (POST)
     Route::post('/permintaanDokter', function (Request $request) {
